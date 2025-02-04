7bf7347bfecfc65bb555f6dd1830b9ee4c7a9a47
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-02-04 22:02:39 +0200

    first edi

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <input type="checkbox" value="dark" className="toggle theme-controller" />
+    </div>
+  );
+};
+
+export default Header;
