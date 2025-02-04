c08af2cc591d7c86f3f8f3562a93cc1ca539fed8
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-02-04 22:06:30 +0200

    second edi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+        officiis.
+      </h1>
       <Header />
     </div>
   );
